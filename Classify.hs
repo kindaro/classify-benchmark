c51dee4cf8a15383ec321b7a4075c556ef4b1f4f
Ignat Insarov <kindaro@gmail.com>
2019-09-02 21:34:22 +0300

    Remove extraneous constraints.

--- Classify.hs
+++ Classify.hs
@@ -1,16 +1,13 @@
-{-# language ScopedTypeVariables #-}
-
 module Classify where
 
 import qualified Data.List as List
 
-classify :: Ord a => [a] -> [[a]]
+classify :: Eq a => [a] -> [[a]]
 classify = classifyBy (==)
 
-classifyBy :: forall a. Ord a => (a -> a -> Bool) -> [a] -> [[a]]
+classifyBy :: (a -> a -> Bool) -> [a] -> [[a]]
 classifyBy eq = List.foldl' f [ ]
   where
-    f :: [[a]] -> a -> [[a]]
     f [ ] y = [[y]]
     f (xs@ (x: _): xss) y | x `eq` y  = (y: xs): xss
                           | otherwise = xs: f xss y
